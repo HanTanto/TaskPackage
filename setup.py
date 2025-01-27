5350021811a41ef0ddd6ec74d9eec3d75a06437c
HanTanto <hanitustanto11@gmail.com>
2025-01-27 23:03:20 +0700

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="TaskPackage",
+    name="TaskPackage_",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[],

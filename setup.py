6cf3ea138c28b67585dad1b083e0e83652566912
HanTanto <hanitustanto11@gmail.com>
2025-01-27 22:49:32 +0700

    setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="TaskPackage",  # Nama unik di PyPI
-    version="0.1",
+    name="TaskPackage",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[],
     author="Hani Tustanto",
-    description="Package contoh untuk tugas",
-    python_requires=">=3.6",
+    author_email="hanitustanto11@gmail.com.com",
+    description="Contoh package untuk tugas",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/HanTanto/TaskPackage",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
+

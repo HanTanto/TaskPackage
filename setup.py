53c2fcb47ab46879c499d30bf0f2af41f5adf5b5
HanTanto <hanitustanto11@gmail.com>
2025-01-27 14:55:04 +0000

    Upload package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TaskPackage",  # Nama unik di PyPI
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Hani Tustanto",
+    description="Package contoh untuk tugas",
+    python_requires=">=3.6",
+    url="https://github.com/HanTanto/TaskPackage",
+)
